36fe82c9c67383bc38f4f99d2472f69e27c40f29
kirillibrahim <59452545+kirillibrahim@users.noreply.github.com>
2022-04-26 18:30:48 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import "./styles.css";
+import List from "./List";
+
+/**
+ * Howdy, and welcome to the Alexis frontend code evaluation!
+ *
+ * To get started, fork this project (either click "Fork" in the
+ * upper right or simply save a change).
+ *
+ * Please complete the following tasks:
+ * - Fetch users and display their avatar and full name in a list
+ * - Display your user in the upper right of the screen (avatar & name)
+ * - Add an input where you can filter on user names
+ * - Make sure your data is typed
+ * - Style the UI at your own discretion ;)
+ *
+ * Endpoints
+ * - List users: https://jonruna.github.io/tapi/userList.json
+ * - Data about your user: https://jonruna.github.io/tapi/me.json
+ *
+ * When done, please send the fork link our way! 🎉
+ */
+
+export default function App() {
+  return <List />;
+}
---
+++ src/List.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ListItem from "./ListItem";
+
+export default function List() {
+  return (
+    <>
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </>
+  );
+}
